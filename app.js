c44ed46ddfdedf9dd980c54e55c6d2c2221cfa91
prashant-pandit <prashantpandit635@gmail.com>
2024-04-16 13:45:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const container = React.createElement('div', { id: 'container' }, [React.createElement('h1', {}, 'Namaste World!W'), React.createElement('h2', {}, 'Namaste h2')])
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(container)
+
